a47c199dd735d10027c2bb2b91d403466e8a3aa9
valeriy <valerabaditsa@gmai.com>
2016-09-17 20:37:33 +0300

    First version

---
+++ Figures.BL/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace Figures.BL
+{
+    public class Rectangle : Shapes
+    {    
+        SolidBrush brush;
+        public override Bitmap Draw(Point p1, Point p2, Color color)
+        {                      
+            pen.Color = color;            
+            graph = Graphics.FromImage(bmp);
+            graph.DrawRectangle(pen, p1.X, p1.Y, p2.X, p2.Y);
+            graph.FillRectangle(brush, new System.Drawing.Rectangle(p1.X, p1.Y, p2.X, p2.Y));            
+            return bmp;
+        }
+
+        public Rectangle(Color backgroundColor)
+        {
+            pen = new Pen(Color.Black);
+            pen.Width = 5;
+            brush = new SolidBrush(backgroundColor);          
+        }
+
+    }
+}
